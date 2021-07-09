a853a51a605b9850cae61fa1f2b5edcefab1e197
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-07-09 14:15:13 +0300

    add coin

--- Assets/Scriptis/PlayerLife.cs
+++ Assets/Scriptis/PlayerLife.cs
@@ -3,19 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+
+    bool dead = false;
+
+    private void Update()
+    {
+        if (transform.position.y < -1f && dead == false)
+        {
+            Die();
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy Body")){
+        if (collision.gameObject.CompareTag("Enemy Body"))
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<PlayerMovement>().enabled = false;
             Die();
         }
     }
 
     private void Die()
     {
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<Rigidbody>().isKinematic = true;
-        GetComponent<PlayerMovement>().enabled = false;
-        ReloadLevel();
+
+        Invoke(nameof(ReloadLevel), 1.3f);
+        dead = true;
     }
 
     private void ReloadLevel()
