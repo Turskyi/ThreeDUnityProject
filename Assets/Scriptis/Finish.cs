0e055e05136aa2d1c5b639dacc6d347a5af19d77
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-07-10 13:37:15 +0300

    add level 2
    Make it an infinite loop

---
+++ Assets/Scriptis/Finish.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            } else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            }
+
+        }
+    }
+}
